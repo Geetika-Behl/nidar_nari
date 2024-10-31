8ca6523dab3c72f5f8f1b07f9c83ffb41ee77809
Ravinder <ravibeniwal931@gmail.com>
2024-10-31 22:01:15 +0530

    Update Cart.js
    i created array of object  replacing object of object in items

--- models/Cart.js
+++ models/Cart.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  items: [
-    {
+  items: {
+    [
       productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
       quantity: { type: Number, default: 1 },
-    },
-  ],
+    ],
+},
   totalQuantity: { type: Number, default: 0 },
   totalPrice: { type: Number, default: 0 },
 });
